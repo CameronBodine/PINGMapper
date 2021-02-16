0569b353834513dd346cc2d9bc2452352e0d7739
Cameron Bodine <csb67@nau.edu>
2021-02-16 08:47:10 -0700

    Added __init__ statement to sonObj

---
+++ c_sonObj.py
@@ -0,0 +1,14 @@
+
+
+from common_funcs import *
+
+class sonObj:
+    def __init__(self, sonFile, humFile, projDir, tempC, nchunk):
+        # Create necessary attributes
+        self.sonFile = sonFile
+        self.humFile = humFile
+        self.projDir = projDir
+        self.tempC = tempC
+        self.nchunk = nchunk
+        
+        return
