14e6dde1be8467c28d81a18bcb6a5210790de880
Cameron Bodine <csb67@nau.edu>
2022-06-22 04:39:32 -0700

    New depth model

--- src/main_readFiles.py
+++ src/main_readFiles.py
@@ -525,7 +525,7 @@ def read_master_func(sonFiles,
         # Zheng et al. 2021
         if detectDep == 1:
             print('\n\tUsing Zheng et al. 2021 method. Loading model...')
-            psObj.weights = r'./models/bedpick/Zheng2021/bedpick_ZhengApproach_20220609.h5'
+            psObj.weights = r'./models/bedpick/Zheng2021/bedpick_ZhengApproach_20220621.h5'
             psObj.configfile = psObj.weights.replace('.h5', '.json')
             # psObj._initModel(USE_GPU)
 
