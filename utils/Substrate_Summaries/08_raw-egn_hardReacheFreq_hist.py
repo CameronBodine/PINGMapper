a0678d161806c7455d574f60025666b3659bb1cc
Cameron Scott Bodine <CameronBodine@nau.edu>
2024-06-06 08:26:38 -0700

    All GS post process utils

---
+++ utils/Substrate_Summaries/08_raw-egn_hardReacheFreq_hist.py
@@ -0,0 +1,239 @@
+
+'''
+***********************************************
+WARNING: Not intended for general use but may 
+serve as a template for creating new workflows.
+***********************************************
+
+Generate histogram comparing count of 1-km reaches
+with x proportion of hard substrates
+'''
+
+
+#########
+# Imports
+import sys, os
+from glob import glob
+import pandas as pd
+import plotnine as p9
+import numpy as np
+
+
+# summaryLengths =[500, 1000, 5000, 10000]
+summaryLengths = [500]
+riverCodes = ['BCH', 'PRL', 'BOU', 'LEA', 'PAS', 'CHI', 'CHU']
+riverNames = ['Bogue Chitto R.', 'Pearl R.', 'Bouie R.', 'Leaf R.', 'Pascagoula R.', 'Chickasawhay R.', 'Chunky R.']
+
+rivCodeName = {'PRL': 'Pearl R.',
+               'BCH': 'Bogue Chitto R.',
+               'PAS': 'Pascagoula R.',
+               'LEA': 'Leaf R.',
+               'CHI': 'Chickasawhay R.',
+               'BOU': 'Bouie R.',
+               'CHU': 'Chunky R.'
+                }
+
+rivBasin = {'PRL': '[PRL Basin]',
+            'BCH': '[PRL Basin]',
+            'PAS': '[PAS Basin]',
+            'LEA': '[PAS Basin]',
+            'CHI': '[PAS Basin]',
+            'BOU': '[PAS Basin]',
+            'CHU': '[PAS Basin]'
+            }
+
+substrateOutputs = ['EGN', 'Raw']
+csvTopDir = '04_Substrate_Shps_Summary_All_Plots'
+outDir = '08_raw-egn_hardReacheFreq_hist'
+
+topDir = r'E:\SynologyDrive\GulfSturgeonProject\SSS_Data_Processed\Substrate_Summaries'
+# topDir = r'S:\GulfSturgeonProject\SSS_Data_Processed\Substrate_Summaries'
+topDir = os.path.normpath(topDir)
+
+
+###########
+# Functions
+    
+def prep_df(df, k):
+
+    # Get column names
+    cols = df.columns.values
+
+    # Get non substrate columns
+    sub_cols = [f for f in cols if k in f]
+    # cols_2_keep = ['river_code', 'rkm', 'sinuosity'] + sub_cols
+    cols_2_keep = ['river_code', 'rkm', 'mapped_area'] + sub_cols
+
+    # Get only the columns needed
+    df = df.loc[:, cols_2_keep]
+
+    # Update basin name
+    for k, v in rivBasin.items():
+        df.loc[df['river_code'] == k, 'basin'] = v
+
+    # Update river name
+    for k, v in rivCodeName.items():
+        df.loc[df['river_code'] == k, 'river_code'] = v
+    
+
+    # Get Hard Proportion
+    df['Hard_prop'] = df['Cobble_Boulder_prop'] + df['Hard_Bottom_prop']
+
+    df = df.fillna(0)
+
+    return df
+    
+
+def df_for_patch_hist(csv):
+
+    # Open
+    df = pd.read_csv(csv)
+
+    # Prep the df
+    df = prep_df(df, 'prop')
+
+    # Only columns we need
+    df = df[['river_code', 'basin', 'mapped_area', 'Hard_prop']]
+
+    df['river_code'] =pd.Categorical(df['river_code'], riverNames)
+
+    df['Hard_prop'] = np.around(df['Hard_prop'], 2)
+
+    return df
+
+
+def makeFreqHist(df, out, length):
+
+    binWidth = 0.05
+    xlim = (0.1, 1.0)
+
+    if length == 500:
+        title = str(np.round(length/1000, 1))
+    else:
+        title = str(int(np.round(length/1000, 0)))
+
+    gg = p9.ggplot(df, p9.aes(x='Hard_prop', color = 'Model'))+\
+         p9.theme_classic()+\
+         p9.theme(figure_size=(4,4))+\
+         p9.geom_freqpoly(binwidth = binWidth)+\
+         p9.xlim(xlim)+\
+         p9.scale_color_grey() +\
+         p9.labels.xlab('Hard Substrate Proportion: {}'.format(xlim))+\
+         p9.labels.ylab('Reach Count')+\
+         p9.ggtitle(title+" km Reach")
+    
+    p9.ggsave(gg, out, dpi=300)
+
+    return
+
+def makeFreqPolyRiver(df, out, length):
+    binWidth = 0.05
+    xlim = (0.1, 1.0)
+
+    if length == 500:
+        title = str(np.round(length/1000, 1))
+    else:
+        title = str(int(np.round(length/1000, 0)))
+
+    gg = p9.ggplot(df, p9.aes(x='Hard_prop', color = 'Model'))+\
+         p9.theme_classic()+\
+         p9.theme(figure_size=(8,4))+\
+         p9.geom_freqpoly(binwidth = binWidth)+\
+         p9.xlim(xlim)+\
+         p9.scale_color_grey() +\
+         p9.labels.xlab('Hard Substrate Proportion: {}'.format(xlim))+\
+         p9.labels.ylab('Reach Count')+\
+         p9.ggtitle(title+" km Reach") +\
+         p9.facet_wrap('river_code', scales='free_y', ncol=4)
+    
+    p9.ggsave(gg, out, dpi=300)
+
+    return
+
+
+def getStats(df, model, prop):
+
+    reachCntTotal = len(df)
+    # Calculate Area in ha
+    df['Hard_Area'] = df['mapped_area'] * df['Hard_prop'] * 0.0001
+    totalArea = np.around(df['Hard_Area'].sum(axis=0), 1)
+
+    print('\nHard Substrate Stats for: {} | Proportion: {}'.format(model, prop))
+    print('Total Patches > \t{}: \t{}'.format(prop, reachCntTotal))
+    print('Total Area [ha] > \t{}: \t{}'.format(prop, totalArea))
+
+    # Get rivers
+    rivs = np.unique(df['river_code'].to_numpy(na_value='-'))
+
+    for riv in rivs:
+        rivdf = df.loc[df['river_code'] == riv]
+        cnt = len(rivdf)
+        area = np.around(rivdf['Hard_Area'].sum(axis=0), 1)
+
+        print('\t{:20s}| Reach Count = {}\t\tArea [ha] = {}'.format(riv, cnt, area))
+
+
+
+#########
+# Do Work
+
+# Iterate each summary length
+for length in summaryLengths:
+
+    for substrateOutput in substrateOutputs:
+        csvDir = os.path.join(topDir, csvTopDir, substrateOutput)
+        csvDir = os.path.normpath(csvDir)
+        
+        outDir = os.path.join(topDir, outDir)
+        outDir = os.path.normpath(outDir)
+
+
+        if not os.path.exists(outDir):
+            os.makedirs(outDir)
+
+        # Get csv
+        csvFile = '_'.join([substrateOutput, 'ALL', str(length), 'summary.csv'])
+        csvFile = os.path.join(csvDir, csvFile)
+
+        # Get dataframe
+        df = df_for_patch_hist(csvFile)
+
+        # Add Model
+        df['Model'] = substrateOutput
+
+        # # Drop less then .1 (?)
+        # df = df.loc[df['Hard_prop'] >= 0.05]
+
+        if 'dfAll' not in locals():
+            dfAll = df
+        else:
+            dfAll = pd.concat([dfAll, df], ignore_index=True)
+
+    
+    # Make fequency poly for all rivers
+    outPlot = '_'.join(['ALL', str(length), 'Raw-EGN_HardReachFreq_hist.png'])
+    outPlot = os.path.join(outDir, outPlot)
+
+    makeFreqHist(dfAll, outPlot, length)
+
+    # Make frequency poly facet wrap by river
+    outPlot = '_'.join(['River', str(length), 'Raw-EGN_HardReachFreq_hist.png'])
+    outPlot = os.path.join(outDir, outPlot)
+
+    makeFreqPolyRiver(dfAll, outPlot, length)
+
+    # Print a summary to report on
+
+    bins = np.arange(0.2, 1, 0.2)
+    for bin in bins:
+        bin = np.around(bin, 1)
+        print('\n\n\nHard Substrate Stats for Proportion > {}'.format(bin))
+        df = dfAll.loc[dfAll['Hard_prop'] > bin]
+
+        raw = df.loc[df['Model'] == 'Raw'].copy()
+        egn = df.loc[df['Model'] == 'EGN'].copy()
+
+        getStats(raw, 'Raw', bin)
+        getStats(egn, 'EGN', bin)
+
+    del dfAll
---
+++ utils/Substrate_Summaries/09_raw-egn_PatchSize_density.py
@@ -0,0 +1,209 @@
+
+'''
+***********************************************
+WARNING: Not intended for general use but may 
+serve as a template for creating new workflows.
+***********************************************
+
+Generate density curves for substrate poly patch size
+faceted by substrate type (or river??)
+'''
+
+#########
+# Imports
+import sys, os
+from glob import glob
+import pandas as pd
+import geopandas as gpd
+import plotnine as p9
+import numpy as np
+
+############
+# Parameters
+# substrateShpsDir = r'S:\GulfSturgeonProject\SSS_Data_Processed\Substrate_Summaries\02_Substrate_Shps_Mosaic_Transects'
+substrateShpsDir = r'E:\SynologyDrive\GulfSturgeonProject\SSS_Data_Processed\Substrate_Summaries\02_Substrate_Shps_Mosaic_Transects'
+outDir = os.path.join(os.path.dirname(substrateShpsDir), '09_raw-egn_PatchSize_density')
+
+outFolderNames = ['Raw', 'EGN']
+
+classOrder = ['Fines Ripple', 'Fines Flat', 'Cobble Boulder', 'Hard Bottom', 'Wood']
+
+rivCodeName = {'PRL': 'Pearl R.',
+               'BCH': 'Bogue Chitto R.',
+               'PAS': 'Pascagoula R.',
+               'LEA': 'Leaf R.',
+               'CHI': 'Chickasawhay R.',
+               'BOU': 'Bouie R.',
+               'CHU': 'Chunky R.'
+                }
+
+if not os.path.exists(outDir):
+    os.makedirs(outDir)
+
+###########
+# Functions
+
+def prep_df(f):
+
+    # Open shapefile
+    df = gpd.read_file(f)
+
+    df = df[['Name', 'Area_m', 'geometry']]
+
+    df = df.rename(columns={'Name': 'Substrate'})
+
+    # Add river name
+    river = os.path.basename(f).split('_')[0]
+    df['River'] = rivCodeName[river]
+
+    return df
+
+def slicePercentile(df, by, l=0.01, u=0.99):
+
+    dfOut = pd.DataFrame()
+    col = 'Area_m'
+
+    for name, group in df.groupby(by):
+        
+        # Get percentiles
+        lp = np.quantile(df[col].to_numpy(), l)
+        up = np.quantile(df[col].to_numpy(), u)
+        
+        group = group.loc[group[col] >= lp]
+        group = group.loc[group[col] <= up]
+
+        dfOut = pd.concat([dfOut, group], ignore_index=True)
+
+    return dfOut
+
+def sliceOutliers(df, by):
+    dfOut = pd.DataFrame()
+    col = 'Area_m'
+
+    for name, group in df.groupby(by):
+        
+        vals = df[col].to_numpy()
+        
+        # # Get percentiles
+        # lp = np.quantile(df[col].to_numpy(), l)
+        # up = np.quantile(df[col].to_numpy(), u)
+
+        q1, q3 = np.percentile(vals, [25,75])
+        iqr = q3-q1
+
+        l = q1 - 1.5*iqr
+        u = q3 + 1.5*iqr
+
+        group = group.loc[group[col] > l]
+        group = group.loc[group[col] < u]
+
+        dfOut = pd.concat([dfOut, group], ignore_index=True)
+    
+    return dfOut
+
+
+
+def makeDensityPlot(df, out, facet, ncol=3):
+
+    # # Slice each facet by percentage
+    # df = slicePercentile(df, facet, l=0, u=0.75)
+
+    # Remove outliers
+    df = sliceOutliers(df, facet)
+
+    gg = p9.ggplot(df, p9.aes(x='Area_m', color='Model'))+\
+         p9.theme_classic()+\
+         p9.theme(figure_size=(12,8))+\
+         p9.geom_density()+\
+         p9.scale_color_grey() +\
+         p9.labels.xlab('Patch Size [m2]')+\
+         p9.facet_wrap(facet, ncol=ncol)
+    
+    p9.ggsave(gg, out, dpi=300)
+
+
+    return
+
+def makeBoxPlot(df, out, facet):
+
+    # # Slice each facet by percentage
+    # df = slicePercentile(df, facet, l=0, u=0.75)
+
+    # Remove outliers
+    df = sliceOutliers(df, facet)
+
+    gg = p9.ggplot(df, p9.aes(x='Model', y='Area_m'))+\
+         p9.theme_bw()+\
+         p9.theme(figure_size=(12,8))+\
+         p9.geom_boxplot()+\
+         p9.labels.ylab('Patch Size [m2]')+\
+         p9.facet_wrap(facet)
+    
+    p9.ggsave(gg, out, dpi=300)
+    return
+
+#########
+# Do Work
+
+shpAll = gpd.GeoDataFrame()
+
+# for dir in outFolderNames:
+
+#     # Get shapefiles
+#     shpFiles = '*_postproc.shp'
+#     shpFiles = os.path.join(substrateShpsDir, dir, shpFiles)
+#     shpFiles = glob(shpFiles)
+
+#     for shp in shpFiles:
+#         shp = prep_df(shp)
+
+#         shp['Model'] = dir
+        
+#         if 'shpAllDir' not in locals():
+#             shpAllDir = shp
+#         else:
+#             shpAllDir = pd.concat([shpAllDir, shp], ignore_index=True)
+
+#     shpAll = pd.concat([shpAll, shpAllDir], ignore_index=True)
+#     del shpAllDir
+
+# shpAllFile = os.path.join(outDir, 'All_River_Model_Maps.shp')
+# shpAll.to_file(shpAllFile)
+
+shpAllFile = os.path.join(outDir, 'All_River_Model_Maps.shp')
+# shpAll = gpd.read_file(shpAllFile)
+
+# # Drop geometry column
+# df = shpAll.drop(columns='geometry')
+# del shpAll
+
+# dfFile = shpAllFile.replace('.shp', '.csv')
+# df.to_csv(dfFile)
+
+dfFile = shpAllFile.replace('.shp', '.csv')
+df = pd.read_csv(dfFile)
+
+# Make density plot: substrate vs patch size
+outPlot = '_'.join(['PatchSize_by_Substrate.png'])
+outPlot = os.path.join(outDir, outPlot)
+
+makeDensityPlot(df, outPlot, 'Substrate', ncol=3)
+
+
+# Make density plot: substrate vs patch size
+outPlot = '_'.join(['PatchSize_by_River.png'])
+outPlot = os.path.join(outDir, outPlot)
+
+makeDensityPlot(df, outPlot, 'River', ncol=3)
+
+# Make Box and Whisker Plot
+outPlot = '_'.join(['Box', 'PatchSize_by_Substrate.png'])
+outPlot = os.path.join(outDir, outPlot)
+
+makeBoxPlot(df, outPlot, 'Substrate')
+
+# Make Box and Whisker Plot
+outPlot = '_'.join(['Box', 'PatchSize_by_River.png'])
+outPlot = os.path.join(outDir, outPlot)
+
+makeBoxPlot(df, outPlot, 'River')
