f57deaa60c021931bf914039a959da9b9c0538ee
Cameron Bodine <cbodine@udel.edu>
2024-11-22 12:01:11 -0500

    Change test defaults

--- test_PINGMapper.py
+++ test_PINGMapper.py
@@ -119,8 +119,8 @@ project_mode = 1
 tempC = 10 #Temperature in Celsius
 nchunk = 500 #Number of pings per chunk
 cropRange = 0.0 #Crop imagery to specified range [in meters]; 0.0==No Cropping
-exportUnknown = False #Option to export Unknown ping metadata
-fixNoDat = True # Locate and flag missing pings; add NoData to exported imagery.
+exportUnknown = True #Option to export Unknown ping metadata
+fixNoDat = False # Locate and flag missing pings; add NoData to exported imagery.
 threadCnt = 0 #Number of compute threads to use; 0==All threads; <0==(Total threads + threadCnt); >0==Threads to use up to total threads
 
 
