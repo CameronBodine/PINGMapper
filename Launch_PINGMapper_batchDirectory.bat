e6bfde66d0a1b5fdb816bf2e69cf0aafc381e467
Cameron Bodine <cbodine@udel.edu>
2024-12-28 04:15:38 -0500

    Add batch files for running pingmapper

---
+++ Launch_PINGMapper_batchDirectory.bat
@@ -0,0 +1,25 @@
+@echo off
+REM Activate the conda environment
+call conda activate ~/miniconda3/envs/ping
+if ERRORLEVEL 1 (
+    goto except_block_lowAnaconda
+)
+goto run_script
+
+:except_block_lowAnaconda
+call conda activate ~/anaconda3/envs/ping
+if ERRORLEVEL 1 (
+    goto except_block_Anaconda
+)
+goto run_script
+
+:except_block_Anaconda
+call conda activate ~/Anaconda3/envs/ping
+if ERRORLEVEL 1 (
+    echo Error! Cannot load Conda environment.
+)
+goto run_script
+
+:run_script
+REM Run the Python script
+python gui_main_batchDirectory.py
