c76e218cb16c89e132444a3ff5badb6b21209b0e
Cameron Bodine <cbodine@udel.edu>
2025-01-03 08:10:30 -0500

    Add default params to setup; v4.0.0a6

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
     packages=find_packages(),
+    data_files=[("default_params", ["pingmapper/default_params.json"])],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
